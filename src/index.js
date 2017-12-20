d73a1c29435d10a8671167cb33895823d89cfba7
gunubin <kato@condesire.com>
2017-12-20 23:29:22 +0900

    feat(scripts): testスクリプト追加

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /* @flow */
 
 type Props = {
-  message: string
+  message1: string,
+  message2: string,
 };
 
 const helloWorld = (props: Props) => {
   // eslint-disable-next-line no-console
-  console.log(props.message);
+  return `${props.message1} ${props.message2}`;
 };
 
 export default helloWorld;
---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+/* @flow */
+import test from 'ava';
+import helloWorld from '../src';
+
+test('hello World!!', async t => {
+  t.is(helloWorld({message1: "hello", message2:"world!!"}), 'hello world!!');
+});
